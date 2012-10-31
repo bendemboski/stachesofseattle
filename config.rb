ae9c3b1f42ecb41e552d45fe73db74cebaa0c290
Ben Demboski <bend@meevine.com>
2012-10-31 12:43:01 -0700

    Better cache busting

--- config.rb
+++ config.rb
@@ -8,3 +8,12 @@ javascripts_dir = "www/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 line_comments = false
+
+asset_cache_buster do |path, real_path|
+  if File.exists?(real_path)
+    pathname = Pathname.new(path)
+    hex = Digest::MD5.hexdigest(File.read(real_path))
+    new_path = "%s?%s" % [path, hex]
+    {:path => new_path, :query => nil}
+  end
+end
