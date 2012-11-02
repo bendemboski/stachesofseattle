071d1e2b92fa037142e3454ec579b131724e489a
Ben Demboski <bend@meevine.com>
2012-11-02 02:06:51 -0700

    v2

--- www/js/all.js
+++ www/js/all.js
@@ -1,3 +1,22 @@
+// Navigation
+$(function() {
+    $(".js-page-nav").click(function(e) {
+        e.preventDefault();
+        var id = $(e.currentTarget).attr("href");
+        $('html,body').animate({
+            scrollTop: $(id).offset().top
+        }, 500);
+    });
+});
+
+// Buy link
+$(function() {
+    $(".js-buy-link").click(function(e) {
+        e.preventDefault();
+        $(e.currentTarget).closest("form").submit();
+    });
+});
+
 $(function() {
   var costBreakdown = $(".js-cost-breakdown");
   costBreakdown.popover({
@@ -8,7 +27,8 @@ $(function() {
      content: '<div class="ui-font-small">' +
          '$4 goes to printing costs<br/>' +
          '$1 goes to PayPal<br/>' +
-         '$10 goes to charity' +
+         '$10 goes to charity<br/>' +
+         '<b>$15 total</b>' +
          '</div>'
   });
   costBreakdown.click(function(e) {
