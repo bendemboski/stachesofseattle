5a8c6c9176fca0c05f643bff1481e58352d9aee2
Ben Demboski <bend@meevine.com>
2012-11-15 09:00:35 -0800

    Welcome modal, calendar pic previews and Amanda's bio

--- www/js/all.js
+++ www/js/all.js
@@ -2,6 +2,7 @@
 $(function() {
     $(".js-page-nav").click(function(e) {
         e.preventDefault();
+        $(e.currentTarget).closest(".modal").modal('hide');
         var id = $(e.currentTarget).attr("href");
         $('html,body').animate({
             scrollTop: $(id).offset().top
@@ -40,4 +41,51 @@ $(function() {
   costBreakdown.click(function(e) {
       e.preventDefault();
   });
+});
+
+$(function() {
+  // Don't know why, but modals are misbehaving
+  $("body").on('show', ".modal", function(e) {
+      var modal = $(e.currentTarget);
+      setTimeout(function() {
+          modal.addClass("in");
+      }, 300);
+  });
+  $("body").on('hide', ".modal", function(e) {
+      setTimeout(function() {
+          var modal = $(e.currentTarget);
+          var data = modal.data("modal");
+          if (data && data.$backdrop) {
+              data.$backdrop.remove();
+          }
+          modal.removeClass("in");
+      }, 300);
+  });
+});
+
+$(function() {
+  $("#welcome-modal").modal('show');
+});
+
+$(function() {
+  var factory = function(placement) {
+    var fn = function() {
+      var el = $(this);
+      el.popover({
+        html: true,
+        trigger: 'hover',
+        placement: placement,
+        animation: false,
+        content: function(e) {
+          return '<img width="' + el.attr("data-width") +
+            '" height="' + el.attr("data-height") +
+            '" src="' + el.attr("href") + '"/>';
+        }
+      });
+    };
+    return fn;
+  };
+
+  $(".js-calendar-top-row .js-calendar-pic-set").each(factory('top'));
+  $(".js-calendar-bottom-row .js-calendar-pic-set").each(factory('top'));
 });
